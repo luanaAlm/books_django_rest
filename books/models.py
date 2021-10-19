c14bd297eb509d21091a0e75e4b6f6c48009865c
Luana Rodirgues <luanarodrigues3211@gmail.com>
2021-10-19 18:10:23 -0300

    react django

---
+++ books/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from uuid import uuid4
+
+
+class Books(models.Model):
+    id_Books = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    release_year = models.IntegerField()
+    state = models.CharField(max_length=50)
+    pages = models.IntegerField()
+    publishing_company = models.CharField(max_length=255)
+    creat_at = models.DateField(auto_now_add=True)
